d48bfa25d066f19648365f0a5dbb518714ce9071
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-06-12 17:30:48 -0300

    Construindo a estrutura do projeto

---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quisquam
+      veritatis, natus itaque consectetur libero cumque repellat, fuga deserunt
+      exercitationem suscipit non nam quaerat inventore ducimus iure ea fugit
+      distinctio?
+    </p>
+  </section>
+)
+
+export default Sobre
