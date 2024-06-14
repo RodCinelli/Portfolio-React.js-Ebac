0faf75d3cf60d54bf086a2a7a379e2435d44e518
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-06-14 01:16:18 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/RodCinelli.png" />
-    <Titulo fontSize={20}>Rod Cinelli</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rod Cinelli</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Rodrigo Cinelli
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Full-Stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
